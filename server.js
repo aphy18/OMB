4a9f8ab828fa5d1cc5d90b89989e4fc9bbab3d27
aphy18 <aphason1@gmail.com>
2022-07-02 10:46:46 -0400

    css files are responsive

--- server.js
+++ server.js
@@ -3,34 +3,41 @@ const app = express();
 const port = 8080;
 const pool = require('./db/db')
 
+// app.use is middleware, runs inbetween the request and response operations
+//every time you run the req/res code, the middleware will be ran as well
+
 app.use(express.json()) // req.body
 
+// this means using the /styles route in reference from the root directory, then specifying the folder name
+app.use('/styles', express.static('styles'));
+
 app.set('view engine', 'ejs');
+
+console.log(__dirname + '\styles')
+
+// res.json sends a json response to the front end
 
 app.get('/', async (req,res) => {
     try {
         const person = await pool.query('SELECT * FROM person WHERE id = 2')
         let personObj = person.rows[0];
         res.render('home', { personObj } )
-        console.log('person -->', person)
+        // console.log('person -->', person)
         console.log('person rows 0 -->', person.rows[0]);
     } catch (err) {
         console.log(err.message)
     }
 })
 
+
+
+
+
 app.get('/deadend', async (req,res) => {
     res.status(400).send('nothing past here')
 })
 
-app.get('/test', async (req,res) => {
-    let student = {
-        name: 'Aphy',
-        age: 20,
-        grades: ['A','B','c']
-    }
-    res.render('test', { student })
-})
+
 
 
 app.listen(port)
