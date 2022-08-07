ae9c03aa182bcd0f0b99065583347f0580cc10a7
aphy18 <aphason1@gmail.com>
2022-08-07 11:55:54 -0400

    next / prev button login

--- scripts/work.js
+++ scripts/work.js
@@ -4,14 +4,32 @@ let workParagraphArray = Array.from(workParagraph);
 let workButtonNext = document.querySelector('.work-button-next');
 let workButtonPrev = document.querySelector('.work-button-prev');
 
-let tracker = -1;
+let tracker = 0;
+
+
 
 workButtonNext.addEventListener('click', () => {
-    tracker++
+    if (tracker < 3) {
+        tracker++;
+        console.log('tracker -->', tracker)
+        for (let i=0; i < workParagraphArray.length; i++) {
+            if (tracker === i + 1) {
+                console.log(`work paragraph ${i + 1} -->`, workParagraphArray[i])
+            }
+        }
+    }
+})
 
-    for (let i=0; i < workParagraphArray.length; i++) {
-        if (tracker === i) {
-            console.log(`work paragraph ${i} -->`, workParagraphArray[i])
+
+
+workButtonPrev.addEventListener('click', () => {
+    if (tracker > 0) {
+        tracker--;
+        console.log('tracker -->', tracker)
+        for (let i=0; i < workParagraphArray.length; i++) {
+            if (tracker === i + 1) {
+                console.log(`work paragraph ${i + 1} -->`, workParagraphArray[i])
+            }
         }
     }
 })
