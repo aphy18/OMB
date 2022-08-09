bc3c70a23cc91e1d6aafd482a7a1c667223304ec
aphy18 <aphason1@gmail.com>
2022-08-09 17:47:27 -0400

    work script complete

--- scripts/work.js
+++ scripts/work.js
@@ -11,6 +11,7 @@ let counter=0;
 
 workButtonNext.addEventListener('click', () => {
     if (counter < 3) {
+        finishWorkButton.setAttribute('disabled','')
         counter++;
         console.log('counter -->', counter)
 
@@ -18,8 +19,13 @@ workButtonNext.addEventListener('click', () => {
         for (let i=0; i < workParagraphArray.length; i++) {
             if (counter >= i + 1) {
                 workParagraphArray[i].style.opacity = 1;
+                // if (workParagraphArray[i].getAttribute('id') === 'paragraph-1' || workParagraphArray[i].getAttribute('id') === 'paragraph-3') {
+                //     workParagraphArray[i].style.transform = 'translateX(50px)'
+                // }
             }
         }
+    } else {
+        finishWorkButton.removeAttribute('disabled')
     }
 })
 
@@ -27,11 +33,17 @@ workButtonNext.addEventListener('click', () => {
 
 workButtonPrev.addEventListener('click', () => {
     if (counter >= 1) {
+        finishWorkButton.setAttribute('disabled','')
         counter--;
         console.log('counter -->', counter)
         for (let i=0; i < workParagraphArray.length; i++) {
             if (counter < i + 1) {
                 workParagraphArray[i].style.opacity = 0;
+                // if (workParagraphArray[i].getAttribute('id') === 'paragraph-1' || workParagraphArray[i].getAttribute('id') === 'paragraph-3') {
+                //     workParagraphArray[i].style.transform = 'translateX(-50px)'
+                // } else {
+                //     workParagraphArray[i].style.transform = 'translateX(5px)'
+                // }
             }
         }
     }
